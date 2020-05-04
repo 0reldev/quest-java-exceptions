ba893e16e72a518a14a550e18ee94715180e3fc0
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-05-04 10:55:03 +0200

    Management of exception

--- src/main/OddSum.java
+++ src/main/OddSum.java
@@ -3,32 +3,41 @@ import java.util.ArrayList;
 public class OddSum {
 
     public static void main(String[] args) {
-        run(args, false);
-    }
+
+        run(args, true);
+}
 
     public static int run(String[] args, boolean test) {
 
-        ArrayList<Integer> listInt = new ArrayList<>();
-        int res;
-        if (!test) {
-            System.out.println("Elements' list :");
-            for (String v : args)
-                System.out.print(" " + v);
-            System.out.println();
-        }
-        for (int i = 0; i < args.length; i++) {
-            listInt.add(Integer.parseInt(args[i]));
+        try {
+
+            ArrayList<Integer> listInt = new ArrayList<>();
+            int res;
+            if (!test) {
+                System.out.println("Elements' list :");
+                for (String v : args)
+                    System.out.print(" " + v);
+                System.out.println();
+            }
+            for (int i = 0; i < args.length; i++) {
+                listInt.add(Integer.parseInt(args[i]));
+            }
+
+            res = sum(listInt);
+            if (test) {
+                return res;
+            }
+
+            System.out.println("Sum of odd elements : " + res);
+            System.out.println("END!");
+
+            return 0;
+
+        } catch (NumberFormatException nfe) {
+            System.out.println("Error: only numbers are accepted.");
+            return -1;
         }
 
-        res = sum(listInt);
-        if (test) {
-            return res;
-        }
-
-        System.out.println("Sum of odd elements : " + res);
-        System.out.println("END!");
-
-        return 0;
     }
 
     public static int sum(ArrayList<Integer> listInt) {
